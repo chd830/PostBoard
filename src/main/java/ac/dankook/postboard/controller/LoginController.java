20757706b12bc04f7d669ca6f20d9443f87c55ab
eunchong <chd830@naver.com>
2018-10-24 20:12:56 +0900

    컨트롤러에서 쿠키를 담지 못함

--- src/main/java/ac/dankook/postboard/controller/LoginController.java
+++ src/main/java/ac/dankook/postboard/controller/LoginController.java
@@ -2,28 +2,25 @@ package ac.dankook.postboard.controller;
 
 import ac.dankook.postboard.data.User;
 import ac.dankook.postboard.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
+    static private Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     UserService userService;
 
     @RequestMapping(value="/")
     public String login() {
+        LOGGER.debug("LogInController");
         return "login";
     }
-//    public Cookie makeCookie(HttpServletResponse response,String userId) {
-//        Cookie cookie = new Cookie("userId",userId);
-//        cookie.setMaxAge(60*60*1);
-//        response.addCookie(cookie);
-//        return cookie;
-//    }
 
     @RequestMapping(value="/rest/login",method= RequestMethod.GET)
     @ResponseBody
