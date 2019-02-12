3014c0dcd221c0fa8394dfbd5bbe2cac8d40ddae
eunchong <chd830@naver.com>
2019-02-13 00:02:20 +0900

    controller에서 model로 15개의 값 출력

--- src/main/java/ac/dankook/postboard/controller/PostController.java
+++ src/main/java/ac/dankook/postboard/controller/PostController.java
@@ -1,19 +1,31 @@
 package ac.dankook.postboard.controller;
 
 import ac.dankook.postboard.constants.UrlConstants;
+import ac.dankook.postboard.service.PostService;
+import ac.dankook.postboard.service.UserService;
 import ac.dankook.postboard.utils.HttpUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Map;
 
 @Controller
 @RequestMapping(UrlConstants.POST)
 public class PostController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PostController.class);
+
+    @Autowired
+    PostService postService;
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping()
     public String post(HttpServletRequest request) {
@@ -34,9 +46,11 @@ public class PostController {
     }
 
     @RequestMapping(value = "/list")
-    public String postList(HttpServletRequest request) {
+    public String postList(Model model, HttpServletRequest request) {
         String userNo = HttpUtils.getUserNoFromCookie(request);
         if (StringUtils.isNotBlank(userNo)) {
+            model.addAttribute("list", postService.getPostByUserNo(userNo));
+            model.addAttribute("userName", userService.getUserName(userNo));
             return "list";
         }
         return "redirect:/";
